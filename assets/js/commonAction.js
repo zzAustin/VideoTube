c9e8cda34d0cb23ff27a166fdf815f366d4f30b3
Zhang Zhong <zhong.zhang@ubisoft.com>
2019-09-18 14:31:21 +0800

    changes

---
+++ assets/js/commonAction.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	console.log("Austin's here!");
+});
--- index.php
+++ index.php
@@ -2,16 +2,24 @@
 <html>
 <head>
 	<title>VideoTube</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><!--Bootstrap css file-->
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><!--jquery file-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><!--Bootstrap js file-->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><!--Bootstrap js file-->
+	<script src="assets/js/commonAction.js"></script>
 </head>
 <body>
 	<div id="pageContainer">
 		<div id="mastHeadContainer">
-			Hello
+			<button class="navShowHide">Click Me!</button>
 		</div>
 		<div id="sideNavContainer">
 		</div>
 		<div id="mainSectionContainer">
+			<div id="mainContentContainer">
+				Hello, everyone!
+			</div>
 		</div>
 	</div>
 </body>
