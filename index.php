3c7202f93f1db8ac0ff2c4918ed40833346a631a
Zhang Zhong <zhong.zhang@ubisoft.com>
2019-09-18 10:16:25 +0800

    some changes

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-sandwitches
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VideoTube</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div id="pageContainer">
+		<div id="mastHeadContainer">
+			Hello
+		</div>
+		<div id="sideNavContainer">
+		</div>
+		<div id="mainSectionContainer">
+		</div>
+	</div>
+</body>
+</html>
