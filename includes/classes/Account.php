45f0274ec0b466c214b97a18b13b20857581f4df
Zhang Zhong <zhong.zhang@ubisoft.com>
2019-09-26 09:28:15 +0800

    message

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -10,11 +10,78 @@ class Account{
 
 	public function register($fn, $ln, $un, $em, $em2, $pwd, $pwd2){
 		$this->validateFirstName($fn);
+		$this->validateLastName($ln);
+		$this->validateUsername($ln);
+		$this->validateEmails($em, $em2);
+		$this->validatePasswords($pwd, $pwd2);
+
+		if(empty($this->errorArray)){
+			$this->insertUserDetails($fn, $ln, $un, $em, $pwd);
+		}
+		else{
+			return false;
+		}
+	}
+
+	public function insertUserDetails($fn, $ln, $un, $em, $pwd){
+		return true;
 	}
 
 	private function validateFirstName($fn){
 		if(strlen($fn) > 25 || strlen($fn) < 2){
 			array_push($this->errorArray, Constants::$firstNameCharacters);
+		}
+	}
+
+	private function validateLastName($ln){
+		if(strlen($ln) > 25 || strlen($ln) < 2){
+			array_push($this->errorArray, Constants::$lastNameCharacters);
+		}
+	}
+
+	private function validateUsername($un){
+		if(strlen($un) > 25 || strlen($un) < 5){
+			array_push($this->errorArray, Constants::$usernameCharacters);
+			return;
+		}
+
+		$query = $this->con->prepare("SELECT username FROM users WHERE username=:un");
+		$query->bindParam(":un", $un);
+		$query->execute();
+		if($query->rowCount() != 0){
+			array_push($this->errorArray, Constants::$usernameTaken);
+		}
+	}
+
+	private function validateEmails($em, $em2){
+		if($em != $em2){
+			array_push($this->errorArray, Constants::$emailsDoNotMatch);
+		}
+
+		if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
+			array_push($this->errorArray, Constants::$emailInvalid);
+		}
+
+		$query = $this->con->prepare("SELECT email FROM users WHERE email=:em");
+		$query->bindParam(":em", $em);
+		$query->execute();
+		if($query->rowCount() != 0){
+			array_push($this->errorArray, Constants::$emailTaken);
+		}
+	}
+
+	private function validatePasswords($pwd, $pwd2){
+		if($pwd != $pwd2){
+			array_push($this->errorArray, Constants::$passwordsDoNotMatch);
+		}
+
+		if(preg_match("/[^A-Za-z0-9]/", $pwd)){
+			array_push($this->errorArray, Constants::$passwordsNotAlphanumeric);
+		}
+
+		if(strlen($pwd) > 30 || strlen($pwd) < 5){
+			array_push($this->errorArray, Constants::$passwordLength);
+			return;
 		}
 	}
 
--- signUp.php
+++ signUp.php
@@ -18,7 +18,12 @@ if(isset($_POST["submitButton"])){
 	$password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
 	$password2 = FormSanitizer::sanitizeFormPassword($_POST["password2"]);
 
-	$account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+	$wasSuccessful = $account->register($firstName, $lastName, $username, $email, $email2, $password, $password2);
+	
+	if($wasSuccessful){
+		// SUCCESS
+		// Redirect user to index page
+	}
 }
 ?>
 
@@ -48,11 +53,24 @@ if(isset($_POST["submitButton"])){
 					<?php echo $account->getError(Constants::$firstNameCharacters);?> <!--error message after submission-->
 
 					<input type="text" name="firstName" placeholder="First Name" autocomplete="off" required>
+
+					<?php echo $account->getError(Constants::$lastNameCharacters);?> 
 					<input type="text" name="lastName" placeholder="Last Name" autocomplete="off" required>
+
+					<?php echo $account->getError(Constants::$usernameCharacters);?>
+					<?php echo $account->getError(Constants::$usernameTaken);?> 
+					<?php echo $account->getError(Constants::$emailInvalid);?> 
 					<input type="text" name="username" placeholder="User Name" autocomplete="off" required>
 
+					<?php echo $account->getError(Constants::$emailsDoNotMatch);?> 
+					<?php echo $account->getError(Constants::$emailInvalid);?> 
+					<?php echo $account->getError(Constants::$emailTaken);?> 
 					<input type="email" name="email" placeholder="Email" autocomplete="off" required>
 					<input type="email" name="email2" placeholder="Confirm email" autocomplete="off" required>
+
+					<?php echo $account->getError(Constants::$passwordsDoNotMatch);?> 
+					<?php echo $account->getError(Constants::$passwordsNotAlphanumeric);?> 
+					<?php echo $account->getError(Constants::$passwordLength);?> 
 					<input type="password" name="password" placeholder="Password" autocomplete="off" required>
 					<input type="password" name="password2" placeholder="Confirm password" autocomplete="off" required>
 
