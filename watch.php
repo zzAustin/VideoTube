627bc839f2a6d00f55eae04159813e90e51a0a30
Zhang Zhong <zhong.zhang@ubisoft.com>
2019-09-27 18:24:34 +0800

    f

---
+++ watch.php
@@ -0,0 +1,10 @@
+<?php require_once("includes/header.php"); ?>
+
+<?php
+if(!isset($_GET["id"])){
+	echo "No url passed into page";
+	exit();
+}
+?>		
+
+<?php require_once("includes/footer.php"); ?>
