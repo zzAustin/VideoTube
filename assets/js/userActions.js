7d1b37c1f8df1048e7d10e35a831688a7d5d3b02
Austin Zhang <austin_chang@sohu.com>
2019-10-02 13:23:04 +0800

    new

---
+++ assets/js/userActions.js
@@ -0,0 +1,3 @@
+function subscribe(userTo, userFrom, button) {
+	
+}
--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,9 @@ $userLoggedInObj = new User($con, $usernameLoggedIn);
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><!--jquery file-->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><!--Bootstrap js file-->
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><!--Bootstrap js file-->
-	<script src="assets/js/commonAction.js"></script><!--My js file-->
+	<!--My js files-->
+	<script src="assets/js/commonActions.js"></script>
+	<script src="assets/js/userActions.js"></script>
 </head>
 <body>
 	<div id="pageContainer">
