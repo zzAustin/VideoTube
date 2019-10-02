663ced305e71501dcc63a6ad24740b55c048cc78
Austin Zhang <austin_chang@sohu.com>
2019-10-02 13:33:08 +0800

    new

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -1,3 +1,11 @@
 function subscribe(userTo, userFrom, button) {
-	
+	if(userTo == userFrom) {
+		alert("You can't subscribe to yourself");
+		return;
+	}
+
+	$.post("ajax/subscribe.php")
+	.done(function() {
+		console.log("done");
+	});
 }
