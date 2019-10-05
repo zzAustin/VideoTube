7c5972556ff7efc587e6b6c5cc7280cb2924536e
Austin Zhang <austin_chang@sohu.com>
2019-10-05 23:49:45 +0800

    go

--- includes/classes/VideoGrid.php
+++ includes/classes/VideoGrid.php
@@ -48,7 +48,15 @@ class VideoGrid {
 	}
 
 	public function createGridHeader($title, $showFilter) {
-		return "";
+		$filter = "";
+		//create filter
+
+		return "<div class='videoGridHeader'>
+						<div class='left'>
+							$title
+						</div>
+						$filter
+				</div>";
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once("includes/header.php"); ?>
 				
-<?php
+<!--<?php
 	/*echo php_uname() . "<br>";
 	echo PHP_OS . "<br>";*/
 	if(isset($_SESSION["userLoggedIn"])){
@@ -9,7 +9,21 @@
 	else{
 		echo "Not logged in";
 	}
-?>
+?>-->
+
+<div class="videoSection">
+	<?php
+		$subscriptionsProvider = new SubscriptionsProvider($con, $userLoggedInObj);
+		$subscriptionVideos = $subscriptionsProvider->getVideos();
+
+		$videoGrid = new VideoGrid($con, $userLoggedInObj->getUsername());
+		if(User::isLoggedIn()) {
+			
+		}
+
+		echo $videoGrid->create(null, "Recommended", false);
+	?>
+</div>
 
 
 <?php require_once("includes/footer.php"); ?>
